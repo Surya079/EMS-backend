457781974a7984356f9d27b724bfa98c6a5c9963
Surya079-V2 <Surya@hazhtech.com>
2024-12-14 10:48:57 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(express.static("public/images"));
+// app.use(express.static("public/images"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/department", departmentRouter);
